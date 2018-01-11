39a14808c9b3f5b081378a32388b9390fca8e21b
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-01-11 14:52:19 +0100

    development of input/output wrappers test and doc

--- tests/input-output.c
+++ tests/input-output.c
@@ -39,19 +39,15 @@ test_1_1 (cce_destination_t upper_L CCSYS_UNUSED)
   if (cce_location(L)) {
     cce_run_error_handlers_raise(L, upper_L);
   } else {
-    ccsys_open_flags_t    flags = { .data = CCSYS_O_CREAT };
-    ccsys_open_mode_t     mode  = { .data = CCSYS_S_IRUSR | CCSYS_S_IWUSR };
-    ccsys_fd_t            fd;
-    fd = ccsys_open(L, "name.ext", flags, mode);
+    static char const *	filename = "name.ext";
+    ccsys_open_flags_t	flags = { .data = CCSYS_O_CREAT };
+    ccsys_open_mode_t	mode  = { .data = CCSYS_S_IRUSR | CCSYS_S_IWUSR };
+    ccsys_fd_t		fd;
+    fd = ccsys_open(L, filename, flags, mode);
     ccsys_cleanup_handler_filedes_init(L, H, fd);
     cctests_assert(0 != fd.data);
+    ccsys_remove(L, filename);
     cce_run_cleanup_handlers(L);
-  }
-
-  if (cce_location(L)) {
-    cce_run_error_handlers_raise(L, upper_L);
-  } else {
-    ccsys_remove(L, "name.ext");
   }
 }
 
@@ -70,7 +66,8 @@ test_2_1 (cce_destination_t upper_L)
     fprintf(stderr, "%s: %s\n", __func__, cce_condition_static_message(cce_condition(L)));
     cce_run_error_handlers_raise(L, upper_L);
   } else {
-    static char const *	dirname = "name.d";
+    static char const * filename = "name.ext";
+    static char const *	dirname  = "name.d";
     ccsys_dirfd_t	dirfd;
 
     /* Create the directory. */
@@ -96,14 +93,14 @@ test_2_1 (cce_destination_t upper_L)
       ccsys_open_mode_t     mode  = { .data = CCSYS_S_IRUSR | CCSYS_S_IWUSR };
       ccsys_fd_t            fd;
 
-      fd = ccsys_openat(L, dirfd, "name.ext", flags, mode);
+      fd = ccsys_openat(L, dirfd, filename, flags, mode);
       ccsys_cleanup_handler_filedes_init(L, file_H, fd);
       cctests_assert(0 != fd.data);
     }
 
     /* Remove the file. */
     {
-      ccsys_unlinkat(L, dirfd, "name.ext", 0);
+      ccsys_unlinkat(L, dirfd, filename, 0);
     }
 
     cce_run_cleanup_handlers(L);
